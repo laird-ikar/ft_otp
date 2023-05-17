6ef644660a807b5cf74628c0d822fc030201cc89
laird-ikar <bguyot@student.42mulhouse.fr>
2023-05-17 13:04:00 +0200

    c'etait trop pro ca faisait peur

--- README.md
+++
@@ -1,5 +0,0 @@
-chmod u+x ./make_env.sh
-./make_env.sh
-source ./env/bin/activate
-
-./ft_otp
--- make_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-python3 -m venv ./env
-source ./env/bin/activate
-python3 -m pip install -r ./requirements.txt
-
-echo "\nVenv installed successfully. You shall now run \"source ./env/bin/activate\", then launch the python script"
